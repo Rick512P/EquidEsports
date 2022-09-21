41d978fbf249b52fa606699d76b0da9687cb2445
Érick <ricksantosmatos@gmail.com>
2022-09-20 21:58:25 -0300

    Create cadastro.php

---
+++ Artigos/cadastro.php
@@ -0,0 +1,17 @@
+<?php
+include_once('artigoDao.php');
+
+    date_default_timezone_set('America/Sao_Paulo');
+
+
+    $titulo1 = $_POST['titulo1'];
+    $subtitulo = $_POST['subtitulo'];
+    $nome_autor = $_POST['nome_autor'];
+    $nome_redator = $_POST['nome_redator'];
+    $paragrafos = $_POST['paragrafos'];
+    $rodape = $_POST['rodape'];
+    $link_original = $_POST['link_original'];
+
+    insereArtigo($titulo1, $subtitulo, $nome_autor, $nome_redator, $paragrafos);
+
+    echo '<a class="botoes cor-gradiente text-white" href="index.php" role="button">Voltar</a>';
