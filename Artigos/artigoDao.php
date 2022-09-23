76ece743cfe88f2ff1f7f93863dd67642fdac3fe
Érick <ricksantosmatos@gmail.com>
2022-09-22 23:55:26 -0300

    Update artigoDao.php

--- Artigos/artigoDao.php
+++ Artigos/artigoDao.php
@@ -27,3 +27,29 @@
         }
     }
 
+    function editaArtigo($idartigos, $titulo1, $subtitulo, $nome_autor, $nome_redator, $paragrafos, $rodape, $link_original)
+    {
+
+        $conexao = criaConexao();
+
+        $sql = "UPDATE artigos set titulo1=:titulo1, subtitulo=:subtitulo, nome_autor=:nome_autor, nome_redator=:nome_redator, paragrafos=:paragrafos, rodape=:rodape , link_original=:link_original where idartigos=:id";
+
+        $stmt = $conexao->prepare($sql);
+        $stmt->bindParam(":id", $idartigos);
+        $stmt->bindParam(":titulo1", $titulo1);
+        $stmt->bindParam(":subtitulo", $subtitulo);
+        $stmt->bindParam(":nome_autor", $nome_autor);
+        $stmt->bindParam(":nome_redator", $nome_redator);
+        $stmt->bindParam(":paragrafos", $paragrafos);
+        $stmt->bindParam(":rodape", $rodape);
+        $stmt->bindParam(":link_original", $link_original);
+
+        try {
+            if ($stmt->execute()) {
+            } else {
+                print_r($stmt->errorInfo());
+            }
+        } catch (PDOException $e) {
+            echo "Erro na conexão. Erro gerado: " . $e->getMessage();
+        }        
+    }
